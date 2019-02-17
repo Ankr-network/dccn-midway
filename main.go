6d92e483a09225194a4ff4495ad1c90ef6ac0a86
Stanley Wu <35579150+fruit37@users.noreply.github.com>
2019-02-17 00:21:37 -0800

    adding cors headers

--- main.go
+++ main.go
@@ -5,24 +5,44 @@ import (
 	"net/http"
 
 	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/mux"
 )
 
 var cache redis.Conn
 
 func main() {
-//	initCache()
-	// "Signin" and "Signup" are handler that we will implement
-	http.HandleFunc("/login", Signin)
-	http.HandleFunc("/signup", Signup)
-	http.HandleFunc("/welcome", Welcome)
-	http.HandleFunc("/refresh", Refresh)
-	http.HandleFunc("/create", CreateTask)
-	http.HandleFunc("/update", UpdateTask)
-	http.HandleFunc("/list", ListTask)
-	http.HandleFunc("/delete", CancelTask)
-	http.HandleFunc("/purge", PurgeTask)
-	http.HandleFunc("/dclist", DataCenterList)
-	http.HandleFunc("/taskdetail", TaskDetail)
-	// start the server on port 8000
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", Signin)
+	r.HandleFunc("/signup", Signup)
+	r.HandleFunc("/welcome", Welcome)
+	r.HandleFunc("/refresh", Refresh)
+	r.HandleFunc("/create", CreateTask)
+	r.HandleFunc("/update", UpdateTask)
+	r.HandleFunc("/list", ListTask)
+	r.HandleFunc("/delete", CancelTask)
+	r.HandleFunc("/purge", PurgeTask)
+	r.HandleFunc("/dclist", DataCenterList)
+	r.HandleFunc("/taskdetail", TaskDetail)
+	http.Handle("/", &MyServer{r})
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type MyServer struct {
+	r *mux.Router
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Add("Access-Control-Allow-Origin", origin)
+		rw.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		rw.Header().Add("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Authorization")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+	// Lets Gorilla work
+	s.r.ServeHTTP(rw, req)
 }
