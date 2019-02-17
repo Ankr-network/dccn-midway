856d11c73efc6cc931117f1440c77d1c944e8979
Tony Wu <xiaowu@berkeley.edu>
2019-02-17 00:09:27 -0800

    [swdev-138]ADD mux

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"github.com/gorilla/mux"
 	"github.com/gomodule/redigo/redis"
 )
 
@@ -12,17 +12,50 @@ var cache redis.Conn
 func main() {
 //	initCache()
 	// "Signin" and "Signup" are handler that we will implement
-	http.HandleFunc("/login", Signin)
-	http.HandleFunc("/signup", Signup)
-	http.HandleFunc("/welcome", Welcome)
-	http.HandleFunc("/refresh", Refresh)
-	http.HandleFunc("/create", CreateTask)
-	http.HandleFunc("/update", UpdateTask)
-	http.HandleFunc("/list", ListTask)
-	http.HandleFunc("/delete", CancelTask)
-	http.HandleFunc("/purge", PurgeTask)
-	http.HandleFunc("/dclist", DataCenterList)
-	http.HandleFunc("/taskdetail", TaskDetail)
+	//http.HandleFunc("/", receiveClientRequest)
+	r := mux.NewRouter()
+/*	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")             
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization") 
+		w.Header().Set("content-type", "application/json")     
+		log.Println("Hello")       
+		return
+})*///log.Println("xiaowang")
+	r.HandleFunc("/login", Signin)
+	r.HandleFunc("/signup", Signup)
+	r.HandleFunc("/welcome", Welcome)
+	r.HandleFunc("/refresh", Refresh)
+	r.HandleFunc("/create", CreateTask)
+	r.HandleFunc("/update", UpdateTask)
+	r.HandleFunc("/list", ListTask)
+	r.HandleFunc("/delete", CancelTask)
+	r.HandleFunc("/purge", PurgeTask)
+	r.HandleFunc("/dclist", DataCenterList)
+	r.HandleFunc("/taskdetail", TaskDetail)
+	//http.HandleFunc("/confirmregistration", confirmRegistration)
+	//http.HandleFunc("/forgotpassword", forgotPassword)
+	//http.HandleFunc("/confirmpassword", confirmPassword)
 	// start the server on port 8000
+	http.Handle("/", &MyServer{r})
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type MyServer struct {
+    r *mux.Router
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+    if origin := req.Header.Get("Origin"); origin != "" {
+	    rw.Header().Add("Access-Control-Allow-Origin", origin)
+        rw.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        rw.Header().Add("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Authorization")
+    }
+    // Stop here if its Preflighted OPTIONS request
+    if req.Method == "OPTIONS" {
+		log.Println("xiaohua")
+        return
+    }
+    // Lets Gorilla work
+    s.r.ServeHTTP(rw, req)
 }
